171a781087131a7f9c5529e6364b05cbcfec02bf
Andrew Marcuse <fileformat@gmail.com>
2018-08-12 17:35:00 -0400

    working at zeit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+LASTMOD=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+grep -q "^LASTMOD=" .env
+if [ $? -ne 0 ]; then
+	echo "LASTMOD=$LASTMOD" >> .env
+else
+	sed -i -e "s/^LASTMOD=.*$/LASTMOD=$LASTMOD/g" .env
+fi
+
+COMMIT=$(git rev-parse --short HEAD)
+grep -q "^COMMIT=" .env
+if [ $? -ne 0 ]; then
+	echo "COMMIT=$COMMIT" >> .env
+else
+	sed -i -e "s/^COMMIT=.*$/COMMIT=$COMMIT/g" .env
+fi
+
+now list vlz-meta-maker
+
+# need to remove old one
+#now rm <URL>
+
+now --public --dotenv  && now alias
+
