e7b8a6971c5735b1987ca8f61ae06894130ee6c1
Andrew Marcuse <fileformat@gmail.com>
2018-08-18 14:52:31 -0400

    code cleanup to remove warnings, normalize spacing

--- server.js
+++ server.js
@@ -3,23 +3,26 @@
 
 // init project
 const express = require('express');
-var fs = require('fs');
-var hbs = require('hbs');
-var os = require('os');
-var rp = require('request-promise-native');
-var request = require('request');
-var bodyParser = require('body-parser');
-var async = require('async');
-var minio = require('minio');
-var multer  = require('multer');
+const fs = require('fs');
+const hbs = require('hbs');
+const os = require('os');
+const rp = require('request-promise-native');
+const request = require('request');
+const bodyParser = require('body-parser');
+const async = require('async');
+const minio = require('minio');
+const multer  = require('multer');
 const Readable = require('stream').Readable;
 const { URL } = require('url');
-var geturls = require('get-urls');
-var yaml = require('js-yaml');
-var psl = require('psl');
-const cheerio = require('cheerio')
+const geturls = require('get-urls');
+const yaml = require('js-yaml');
+const psl = require('psl');
+const cheerio = require('cheerio');
 
-var app = express();
+// hack for nodejs SSL error: https://github.com/nodejs/node/issues/16196
+require("tls").DEFAULT_ECDH_CURVE = "auto";
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'hbs');
@@ -81,243 +84,183 @@ function getStatus() {
 	return retVal;
 }
 
-// true if there is a favicon at this url
-async function checkFavicon(res, url) {
-  var options = {
-    url: url,
-    encoding: null,
-    headers: {
-      'User-Agent': USER_AGENT
-    },
-    resolveWithFullResponse: true,
-    timeout: 1500
-  };
-  
-  var response;
-  try {
-    response = await rp(options);
-    
-  }
-  catch (err) {
-    var errmsg = (err.name == "StatusCodeError" && err.statusCode == 404) ? "404" : JSON.stringify(err);
-    res.write("WARNING: unable to load favicon from '" + url + "' (err=" + errmsg + ")");
-    return false;
-  }
-  var buf = response.body;
-  var mimetype = response.headers['content-type'];
-  if (!buf || buf.length == 0) {
-    return false;
-  }
-  if (mimetype.startsWith("image/") == false) {
-    res.write("WARNING: favicon exists but not an image (mimetype=" + mimetype + ")");
-    return false;
-  }
-  return true;
-}
-
-function findFaviconURL(res, base, html) {
-  const $ = cheerio.load(html);
-  
-  var links = $('link[rel="shortcut icon"]');
-  if (links.length == 0) {
-    links = $('link[rel="icon"]');
-  }
-  
-  if (links.length == 0) {
-    return null;
-  }
-  
-  //var link = cheerio.load(links[0]);
-  //console.log(links[0]);
-  //return null;
-  
-  return new URL(links.attr("href"), base).href;
-}
-
 function isBlog(url) {
-  if (url.host.startsWith("blog.") || url.pathname.endsWith("/blog") || url.pathname.endsWith("/blog/")) {
-    return true;
-  }
-  return false;
+    if (url.host.startsWith("blog.") || url.pathname.endsWith("/blog") || url.pathname.endsWith("/blog/")) {
+        return true;
+    }
+    return false;
 }
 
 function isFacebook(url) {
-      if (url.host.endsWith("facebook.com")) {
-        if (url.pathname == '/' 
+    if (url.host.endsWith("facebook.com")) {
+        if (url.pathname == '/'
             || url.pathname == '/tr'
             || url.pathname.endsWith("/fbml")
             || url.pathname.startsWith("/sharer")
-           ) {
-          return false;
+        ) {
+            return false;
         }
         return true;
-      }
-  return  false;
+    }
+    return  false;
 }
 
 function isGithub(url) {
-  if (url.host.endsWith("github.com") == false) {
-    return false;
-  }
-  
-  if (url.pathname.match("^/[-A-Za-z0-9]+(/[-A-Za-z0-9]+)?$")) {
-    return true;
-  }
-  return  false;
+    if (url.host.endsWith("github.com") == false) {
+        return false;
+    }
+
+    if (url.pathname.match("^/[-A-Za-z0-9]+(/[-A-Za-z0-9]+)?$")) {
+        return true;
+    }
+    return  false;
 }
 
-function isLinkedIn(url) { 
-  if (url.hostname != "linkedin.com" && url.hostname != "www.linkedin.com") {
-    return false;
-  }
-  console.log("LI=" + JSON.stringify(url.pathname));
-  if (url.pathname == '/'  || url.pathname == '/shareArticle') {
-    return false;
-  }
-  return true;
-} 
+function isLinkedIn(url) {
+    if (url.hostname != "linkedin.com" && url.hostname != "www.linkedin.com") {
+        return false;
+    }
+    if (url.pathname == '/'  || url.pathname == '/shareArticle') {
+        return false;
+    }
+    return true;
+}
 
 function isTwitter(url) {
-  if (!url.host.endsWith("twitter.com")) {
-    return false;
-  }
-  if (url.host == "platform.twitter.com") {
-    return false;
-  }
-  if (url.pathname == '/' || url.pathname.startsWith("/intent/")) {
-    return false;
-  }
-  return true;
+    if (!url.host.endsWith("twitter.com")) {
+        return false;
+    }
+    if (url.host == "platform.twitter.com") {
+        return false;
+    }
+    if (url.pathname == '/' || url.pathname.startsWith("/intent/")) {
+        return false;
+    }
+    return true;
 }
 
 function onlyPathname(url) {
-  return url.pathname.substring(1);
+    return url.pathname.substring(1);
 }
 
-var socialSites = [
-  { id: "blog", fn: isBlog },
-  { id: "dribbble", fn: function(url) { return url.hostname == "dribbble.com" } },
-  { id: "facebook", fn: isFacebook },
-  { id: "flickr", fn: function(url) { return url.hostname == "www.flickr.com" } },
-  { id: "github", fn: isGithub, cleanup: onlyPathname },
-  { id: "googleplus", fn: function(url) { return url.hostname == "plus.google.com" } },
-  { id: "instagram", fn: function(url) { return url.hostname == "instagram.com" || url.hostname == "www.instagram.com" } },
-  { id: "linkedin", fn: isLinkedIn },
-  { id: "pinterest", fn: function(url) { return url.hostname == "pinterest.com" ||  url.hostname == "www.pinterest.com" } },
-  { id: "reddit", fn: function(url) { return url.hostname == "www.reddit.com" } },
-  { id: "tumblr", fn: function(url) { return url.hostname == "www.tumblr.com" } },
-  { id: "twitter", fn: isTwitter, cleanup: onlyPathname },
-  { id: "wikipedia", fn: function(url) { return url.hostname == "en.wikipedia.org" } },
-  { id: "youtube", fn: function(url) { return (url.hostname == "youtube.com" || url.hostname == "www.youtube.com") && url.pathname != '/' } },
-  ];
+const socialSites = [
+    { id: "blog", fn: isBlog },
+    { id: "dribbble", fn: function(url) { return url.hostname == "dribbble.com" } },
+    { id: "facebook", fn: isFacebook },
+    { id: "flickr", fn: function(url) { return url.hostname == "www.flickr.com" } },
+    { id: "github", fn: isGithub, cleanup: onlyPathname },
+    { id: "googleplus", fn: function(url) { return url.hostname == "plus.google.com" } },
+    { id: "instagram", fn: function(url) { return url.hostname == "instagram.com" || url.hostname == "www.instagram.com" } },
+    { id: "linkedin", fn: isLinkedIn },
+    { id: "pinterest", fn: function(url) { return url.hostname == "pinterest.com" ||  url.hostname == "www.pinterest.com" } },
+    { id: "reddit", fn: function(url) { return url.hostname == "www.reddit.com" } },
+    { id: "tumblr", fn: function(url) { return url.hostname == "www.tumblr.com" } },
+    { id: "twitter", fn: isTwitter, cleanup: onlyPathname },
+    { id: "wikipedia", fn: function(url) { return url.hostname == "en.wikipedia.org" } },
+    { id: "youtube", fn: function(url) { return (url.hostname == "youtube.com" || url.hostname == "www.youtube.com") && url.pathname != '/' } },
+];
 
 
 app.get('/status.json', function(req, res) {
-  res.writeHead(200, {
+    res.writeHead(200, {
         "Content-Type": "text/plain",
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': 'POST, GET',
         'Access-Control-Max-Age': '604800',
-      });
+    });
 
-  sendJson(req, res, getStatus());
-  return;
+    sendJson(req, res, getStatus());
 });
 
 const asyncMiddleware = fn =>
-  (req, res, next) => {
-    Promise.resolve(fn(req, res, next))
-      .catch(next);
-  };
+    (req, res, next) => {
+        Promise.resolve(fn(req, res, next))
+            .catch(next);
+    };
 
 app.get('/robots.txt', function(req, res) {
-  
-  res.setHeader("content-type", "text/plain; charset=utf-8");
-  res.write("User-Agent: *\n");
-  res.write("Disallow: /\n");
-  res.end();
-  
+
+    res.setHeader("content-type", "text/plain; charset=utf-8");
+    res.write("User-Agent: *\n");
+    res.write("Disallow: /\n");
+    res.end();
+
 });
 
 app.get('/favicon.ico', function(req, res) {
-  request.get(process.env.FAVICON_ICO).pipe(res);
+    request.get(process.env.FAVICON_ICO).pipe(res);
 });
 
 app.get('/favicon.svg', function(req, res) {
-  request.get(process.env.FAVICON_SVG).pipe(res);
+    request.get(process.env.FAVICON_SVG).pipe(res);
 });
 
 app.get('/', function(req, res) {
-  res.render("index", { "step": "url" });
-  return;
+    res.render("index", { "step": "url" });
+    return;
 });
 
 app.post('/', multer({ storage: multer.memoryStorage() }).single('file'), asyncMiddleware(async (req, res, next) => {
-  
-    var buf = null;
-    var mimetype = null;
-  
-    //res.setHeader("content-type", "text/plain; charset=utf-8");
+
     if (req.body["step"] == "confirm") {
-      var logodata = Object.assign({ "created": new Date().toISOString() }, req.body);
-      delete logodata.step;
-      socialSites.forEach(function(ss) {
-        if (logodata[ss.id] == "") {
-          delete logodata[ss.id];
+        var logodata = Object.assign({ "created": new Date().toISOString() }, req.body);
+        delete logodata.step;
+        socialSites.forEach(function(ss) {
+            if (logodata[ss.id] == "") {
+                delete logodata[ss.id];
+            }
+        });
+        console.log("logodata=" + JSON.stringify(logodata));
+        var s = new Readable;
+        s.push(yaml.safeDump(logodata, { lineWidth: 4096, noRefs: true, sortKeys: true }));
+        s.push(null);
+        try {
+            await minioClient.putObject(process.env.S3_BUCKET, logodata.logohandle + ".yaml", s);
+            res.write("SUCCESS!");
+        } catch (err) {
+            console.error(err);
+            res.write("FAILED: " + err.message);
         }
-      });
-      console.log("logodata=" + JSON.stringify(logodata));
-      var s = new Readable;
-      s.push(yaml.safeDump(logodata, { lineWidth: 4096, noRefs: true, sortKeys: true }));
-      s.push(null);
-      try {
-          await minioClient.putObject(process.env.S3_BUCKET, logodata.logohandle + ".yaml", s);
-          res.write("SUCCESS!");
-      } catch (err) {
-          console.error(err);
-          res.write("FAILED: " + err.message);
-      }
-      res.end();
-      return;
+        res.end();
+        return;
     }
-  
-    var url = req.body["url"];
+
+    const url = req.body["url"];
     if (url == null) {
-      res.write("ERROR: url is required");
-      res.end();
-      return;
+        res.write("ERROR: url is required");
+        res.end();
+        return;
     }
-  
-    var mainURL = new URL(url);
-  
-    var options = {
-      url: url,
-      //encoding: null,
-      headers: {
-        'User-Agent': USER_AGENT
-      },
-      resolveWithFullResponse: true,
-      timeout: 10000  // in millis
+
+    const mainURL = new URL(url);
+
+    const options = {
+        url: url,
+        //encoding: null,
+        headers: {
+            'User-Agent': USER_AGENT
+        },
+        resolveWithFullResponse: true,
+        timeout: 10000  // in millis
     };
-    var response = await rp(options);
-    buf = response.body;
-    mimetype = response.headers['content-type'];
-  
-    var messages = [];
-  
-    var metadata = { "website": response.request.uri.href };
+    const response = await rp(options);
+    const buf = response.body;
+
+    const messages = [];
+
+    const metadata = { "website": response.request.uri.href };
     if (url != response.request.uri.href) {
         metadata.originalurl = url;
     }
 
-    var match = buf.match(new RegExp("<title>(.*)</title>"));
+    const match = buf.match(new RegExp("<title>(.*)</title>"));
     if (match != null) {
         metadata.title = match[1];
     } else {
         messages.push("WARNING: no title");
     }
 
-    var urls = geturls(buf);
+    const urls = geturls(buf);
 
     urls.forEach(function(strURL) {
         //res.write("url=" + strURL + "\n");
@@ -329,7 +272,7 @@ app.post('/', multer({ storage: multer.memoryStorage() }).single('file'), asyncM
 
             socialSites.forEach(function (theSite) {
                 if (theSite.fn(theURL)) {
-                    var siteURL = theSite.cleanup ? theSite.cleanup(theURL) : theURL.href;
+                    const siteURL = theSite.cleanup ? theSite.cleanup(theURL) : theURL.href;
                     if (metadata[theSite.id]) {
                         metadata['alt' + theSite.id].push(siteURL);
                     }
@@ -345,7 +288,7 @@ app.post('/', multer({ storage: multer.memoryStorage() }).single('file'), asyncM
         }
     });
 
-    var parsed = psl.parse(mainURL.host);
+    const parsed = psl.parse(mainURL.host);
     metadata.sort = parsed.sld;
     if (parsed.tld == "com" || parsed.tld == "org") {
         metadata.logohandle = parsed.sld
@@ -353,39 +296,39 @@ app.post('/', multer({ storage: multer.memoryStorage() }).single('file'), asyncM
     else {
         metadata.logohandle = parsed.domain.replace('.', '');
     }
-  
+
     if (req.body["step"] == "url") {
         res.render("confirm", { metadata: metadata, socialsites: socialSites, step: "confirm" });
     }
 }));
 
 app.use(function (req, res, next) {
-  res.setHeader("content-type", "text/plain; charset=utf-8");
-  res.status(404).send("404: unable to find file '" + req.url + "'");
+    res.setHeader("content-type", "text/plain; charset=utf-8");
+    res.status(404).send("404: unable to find file '" + req.url + "'");
 });
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack);
-  res.setHeader("content-type", "text/plain; charset=utf-8");
-  res.status(500).send("500: " + err);
+    console.error(err.stack);
+    res.setHeader("content-type", "text/plain; charset=utf-8");
+    res.status(500).send("500: " + err);
 });
 
 function sendJson(req, res, jsonObj) {
-	if ('callback' in req.query)
-	{
-		res.write(req.query["callback"]);
-		res.write("(");
-		res.write(JSON.stringify(jsonObj));
-		res.write(");");
-	}
-	else
-	{
-		res.write(JSON.stringify(jsonObj));
-	}
-  res.end();
+    if ('callback' in req.query)
+    {
+        res.write(req.query["callback"]);
+        res.write("(");
+        res.write(JSON.stringify(jsonObj));
+        res.write(");");
+    }
+    else
+    {
+        res.write(JSON.stringify(jsonObj));
+    }
+    res.end();
 }
 
-var listener = app.listen(process.env.PORT || 4000, function () {
+const listener = app.listen(process.env.PORT || 4000, function () {
     console.log('Listening on port ' + listener.address().port);
 });
 
